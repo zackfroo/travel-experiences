224a9fbf783cea17c044ce19bc3e9f392fd3e606
Zakaria Frouni <zfrouni@gmail.com>
2021-05-12 21:35:18 -0400

    initial commit

---
+++ src/test/java/com/travelguide/travelguide/TravelGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelguide.travelguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
