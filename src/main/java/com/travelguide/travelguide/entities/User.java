4a7b3fbaccb58f4e4b28b92887d435001efb138d
yahyani.mohamed <yahyani.mohamed@courrier.uqam.ca>
2021-05-14 08:16:32 -0400

    Add entities(User, Activity and Category)

---
+++ src/main/java/com/travelguide/travelguide/entities/User.java
@@ -0,0 +1,114 @@
+package com.travelguide.travelguide.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	
+	private Long userId;
+	private String firstName;
+	private String lastName;
+	private String sexe;
+	private String emailAddress;
+	private String phoneNumber;
+	private String country;
+	private String city;
+	private String cardId;
+	
+	public User() {	
+		
+	}
+	
+	public User(String firstName, String lastName, String sexe, String emailAddress, String phoneNumber, String country,
+			String city, String cardId) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.sexe = sexe;
+		this.emailAddress = emailAddress;
+		this.phoneNumber = phoneNumber;
+		this.country = country;
+		this.city = city;
+		this.cardId = cardId;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getSexe() {
+		return sexe;
+	}
+
+	public void setSexe(String sexe) {
+		this.sexe = sexe;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCardId() {
+		return cardId;
+	}
+
+	public void setCardId(String cardId) {
+		this.cardId = cardId;
+	}
+	
+	
+}
