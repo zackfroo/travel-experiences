f97498288b65dc3a228439b1561cd5cb3e593eb8
yahyani.mohamed <yahyani.mohamed@courrier.uqam.ca>
2021-05-23 01:09:32 -0400

    Clean the code

--- src/main/java/com/travelguide/travelguide/TravelGuideApplication.java
+++ src/main/java/com/travelguide/travelguide/TravelGuideApplication.java
@@ -1,25 +1,14 @@
 package com.travelguide.travelguide;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class TravelGuideApplication implements CommandLineRunner {
+public class TravelGuideApplication {
 	
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
-
 	public static void main(String[] args) {
 		SpringApplication.run(TravelGuideApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
